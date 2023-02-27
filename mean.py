012400480c0a5e51fa20eff538a96efd07c64c84
IkbalKhz <100201974+IkbalKhz@users.noreply.github.com>
2023-02-27 23:02:15 +0800

    Add files via upload

---
+++ mean.py
@@ -0,0 +1,6 @@
+nim = 0 + 2 + 2 + 1 + 4 + 5
+n = 6
+hasil = round(nim / n, 2)
+
+# round() untuk membulatkan hasil ke beberapa digit desimal yang diinginkan
+print("hasil dari mean yaitu : ", hasil)
