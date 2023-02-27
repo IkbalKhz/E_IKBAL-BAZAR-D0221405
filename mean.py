1cce8b8b66dddf4ed0cf732655ed023b3fe58d00
IkbalKhz <100201974+IkbalKhz@users.noreply.github.com>
2023-02-27 23:06:42 +0800

    Delete mean.py

--- mean.py
+++
@@ -1,6 +0,0 @@
-nim = 0 + 2 + 2 + 1 + 4 + 5
-n = 6
-hasil = round(nim / n, 2)
-
-# round() untuk membulatkan hasil ke beberapa digit desimal yang diinginkan
-print("hasil dari mean yaitu : ", hasil)
